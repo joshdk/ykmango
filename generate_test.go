82c61625f72bf2e9f0de175d70fea6f8774a4abb
Josh Komoroske <jdkomo@gmail.com>
2017-10-22 16:34:10 -0700

    Added tests for generation

---
+++ generate_test.go
@@ -0,0 +1,159 @@
+// Copyright 2017 Josh Komoroske. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE.txt file.
+
+package ykman
+
+import (
+	//"errors"
+	"errors"
+	"fmt" //"os/exec"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateParse(t *testing.T) {
+
+	var errorGeneric = errors.New("generic error")
+
+	tests := []struct {
+		title         string
+		name          string
+		body          string
+		error         error
+		expectedError error
+		expectedCode  string
+	}{
+		{
+			title:         "empty results",
+			expectedError: ErrorUnknownName,
+		},
+		{
+			title: "single result",
+			name:  "aws",
+			body: `
+				aws  123456
+			`,
+			expectedCode: "123456",
+		},
+		{
+			title: "single long result",
+			name:  "aws",
+			body: `
+				aws  12345678
+			`,
+			expectedCode: "12345678",
+		},
+		{
+			title: "multiple results",
+			name:  "aws",
+			body: `
+				aws  123456
+				aws  867530
+			`,
+			expectedCode: "123456",
+		},
+		{
+			title: "code too short",
+			name:  "aws",
+			body: `
+				aws  1234
+			`,
+			expectedError: ErrorUnknownName,
+		},
+		{
+			title: "bad code format",
+			name:  "aws",
+			body: `
+				aws  DEADBEEF
+			`,
+			expectedError: ErrorUnknownName,
+		},
+		{
+			title: "different name returned",
+			name:  "aws",
+			body: `
+				aws-us-gov  123456
+			`,
+			expectedError: ErrorUnknownName,
+		},
+		{
+			title: "error ykman executable missing",
+			error: &exec.Error{
+				Err: exec.ErrNotFound,
+			},
+			expectedError: ErrorYkmanNotFound,
+		},
+		{
+			title: "error no yubikey detected",
+			body: `
+				Usage: ykman [OPTIONS] COMMAND [ARGS]...
+
+				Error: Failed connecting to the YubiKey.
+			`,
+			error:         errorGeneric,
+			expectedError: ErrorNotDetected,
+		},
+		{
+			title: "error aborted",
+			body: `
+				Touch your YubiKey...
+				^C
+				Aborted!
+			`,
+			error:         errorGeneric,
+			expectedError: ErrorAborted,
+		},
+		{
+			title: "error yubikey removed",
+			body: `
+				Touch your YubiKey...
+				Traceback (most recent call last):
+				  File "/usr/local/bin/ykman", line 11, in <module>
+					load_entry_point('yubikey-manager==0.4.5', 'console_scripts', 'ykman')()
+				  <...snip...>
+				  File "/usr/local/Cellar/ykman/0.4.5/libexec/lib/python2.7/site-packages/ykman/driver_ccid.py", line 123, in send_apdu
+					raise CCIDError(e)
+				ykman.driver_ccid.CCIDError: Failed to transmit with protocol T1. Transaction failed.
+			`,
+			error:         errorGeneric,
+			expectedError: ErrorRemoved,
+		},
+		{
+			title: "error yubikey timed out",
+			body: `
+				Touch your YubiKey...
+				Traceback (most recent call last):
+				  File "/usr/local/bin/ykman", line 11, in <module>
+					load_entry_point('yubikey-manager==0.4.5', 'console_scripts', 'ykman')()
+				  <...snip...>
+				  File "/usr/local/Cellar/ykman/0.4.5/libexec/lib/python2.7/site-packages/ykman/oath.py", line 203, in send_apdu
+					raise APDUError(resp, sw)
+				ykman.driver_ccid.APDUError: APDU error: SW=0x6982
+			`,
+			error:         errorGeneric,
+			expectedError: ErrorTimeout,
+		},
+		{
+			title:         "generic error",
+			error:         errorGeneric,
+			expectedError: errorGeneric,
+		},
+	}
+
+	for index, test := range tests {
+
+		name := fmt.Sprintf("case #%d - %s", index, test.title)
+
+		t.Run(name, func(t *testing.T) {
+			actualCode, actualError := parseGenerate(test.body, test.error, test.name)
+
+			assert.Equal(t, test.expectedError, actualError)
+
+			assert.Equal(t, test.expectedCode, actualCode)
+		})
+	}
+
+}
